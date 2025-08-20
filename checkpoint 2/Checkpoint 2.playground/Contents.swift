bdee35168539a3d7c3e43a6d91dde2a4f4b8ca03
Hunter Forshee <hunter.forshee@stu.barren.kyschools.us>
2025-08-20 09:21:58 -0500

    Checkpoint 2 complete

---
+++ checkpoint 2/Checkpoint 2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let games = ["fallout", "Minecraft", "Deep Rock Galactic", "roblox", "roblox", "fallout"]
+
+let Gnum = games.count
+
+let Gameset = Set(games)
+
+let GSnum = Gameset.count
+
+print(Gnum, GSnum)
