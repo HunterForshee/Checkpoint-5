4f7a1afe2f5d37cfc1adbc47843840cec79f8afe
Hunter Forshee <hunter.forshee@stu.barren.kyschools.us>
2025-09-02 08:49:05 -0500

    Checkpoint 5 complete

--- checkpoint 5/Checkpoint 5.playground/Contents.swift
+++ checkpoint 5/Checkpoint 5.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+luckyNumbers.filter{ (number: Int) -> Bool in
+    number % 2 != 0
+}.sorted().map { (number: Int) -> String in
+    "\(number) is a lucky number!"
+}.map { (message: String) in
+    print(message)
+}
+
